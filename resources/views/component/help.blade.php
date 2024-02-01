ef454bb9c149ae43e1ed2fdccb80805ac3ec8f14
RizkyyAl-Amin <rizkyalamin7224@gmail.com>
2024-02-01 21:06:16 +0700

    revisi halaman bantuan

--- resources/views/component/help.blade.php
+++ resources/views/component/help.blade.php
@@ -23,17 +23,27 @@
                 <h1>Bantuan</h1>
                 <br>
                 <ol class="angka">
-                    <li class="nomor">Membuka Situs E-Laundy
+                    <li class="nomor">Membuka Situs E-Laundry
                         <ul class="bullet">
-                            <li>Buka situs E-Laundy melalui link berikut</li>
-                            <li>Ketika membuka link tersebut, maka muncul halaman login</li>
-                            <li>Untuk login, baca bagian login ke E-Laundry</li>
+                            <li>Buka situs E-Laundry melalui link berikut</li>
+                            <li>Ketika membuka link tersebut, maka muncul halaman selamat datang</li>
+                            <li>Terdapat dua pilihan masuk dan daftar</li>
+                            <li>Jika belum mempunyai akun, maka silahkan klik tombol daftar</li>
                         </ul>
                     </li>
-                    <li class="nomor">Login Ke E-Laundry
+                    <li class="nomor">Daftar Akun E-Laundry
                         <ul class="bullet">
-                            <li>Hubungi developer melalui email <a href="mailto:e.laundry.contact@gmail.com">e.laundry.contact@gmail.com</a> untuk meminta username dan password</li>
-                            <li>Masukkan Username dan Password yang telah diberikan ketika membuka website</li>
+                            <li>Masukan nama anda</li>
+                            <li>Masukan Username dan Password yang sulit ditebak</li>
+                            <li>Lalu konfirmasi ulang Password supaya tidak terjadi kekeliruan dalam pengisian Password</li>
+                            <li>Anda berhasil membuat akun E - Laundry</li>
+                            <li>Selanjutnya akan diarahkan lansung ke halaman masuk</li>
+                        </ul>
+                    </li>
+                    
+                    <li class="nomor">Masuk Ke E-Laundry
+                        <ul class="bullet">
+                            <li>Masukkan Username dan Password yang telah dibuat tadi</li>
                             <li>Anda Berhasil masuk ke perangkat E-Laundry berbasis web</li>
                         </ul>
                     </li>
@@ -43,9 +53,34 @@
                             <li>Di dalam fitur dasbor, terdapat informasi terkait jumlah transaksi yang masuk per bulan dan per minggu</li>
                         </ul>
                     </li>
+                    <li class="nomor">Fungsi Fitur Tambah Kategori Pada Halaman Kategori
+                        <ul class="bullet">
+                            <li>Navigasi ke halaman <b>Kategori</b></li>
+                            <li>Klik tombol <strong>Tambah Kategori</strong> untuk menampilkan formulir Tambah Kategori</li>
+                            <li>Isi jenis Kategori yang diperlukan untuk menambahkan transaksi baru</li>
+                            <li>Simpan Kategori dan akan ada notifikasi setelah berhasil menambahkan kategori</li>
+                        </ul>
+                    </li>
+                    <li class="nomor">Fungsi Fitur Edit Kategori Pada Halaman Kategori
+                        <ul class="bullet">
+                            <li>Temukan dan pilih kategori yang ingin diedit</li>
+                            <li>Pilih opsi edit dan lakukan perubahan sesuai kebutuhan</li>
+                            <li>Simpan perubahan dan akan ada notifikasi Kategori berhasil diedit</li>
+                        </ul>
+                    </li>
+                    <li class="nomor">Fungsi Fitur Hapus Pada Halaman Kategori Transaksi
+                        <ul class="bullet">
+                            <li>Temukan dan pilih kategori yang ingin dihapus</li>
+                            <li>Pilih opsi hapus pada kolom aksi</li>
+                            <li>Kategori akan terhapus dan akan ada notifikasi data berhasil dihapus</li>
+                        </ul>
+                    </li>
+
                     <li class="nomor">Fungsi Fitur Tambah Data Pada Halaman Data Transaksi
                         <ul class="bullet">
                             <li>Navigasi ke halaman <b>Data Transaksi</b></li>
+                            <li>Jika anda user baru, maka halaman data transaksi akan kosong</li>
+                            <li>Maka sebaiknya dianjurkan untuk membuat kategori pesanan terlebih dahulu</li>
                             <li>Klik tombol <strong>Tambah Data</strong> untuk menampilkan formulir Tambah Data</li>
                             <li>Isi formulir atau data yang diperlukan untuk menambahkan transaksi baru</li>
                             <li>Simpan data dan akan ada notifikasi setelah berhasil menambahkan data</li>
@@ -81,7 +116,7 @@
                     </li>
                 </ol>
             </div>
-            <footer>Create at 2023;&copy; CADMAN.
+            <footer>Create at 2024;&copy; KEL7.
                 <p></p>
             </footer>
         </div>
@@ -93,25 +128,25 @@
             <button class="close" id="tidak">Tidak</button>
         </div>
     </div>
-<script>
-var keluar = document.getElementById("keluar");
-var tidak = document.getElementById("tidak");
+    <script>
+        var keluar = document.getElementById("keluar");
+        var tidak = document.getElementById("tidak");
 
-tidak.onclick = function() {
-    keluar.style.display = "none";
-}
+        tidak.onclick = function() {
+            keluar.style.display = "none";
+        }
 
-function openKeluarPopup() {
-    keluar.style.display = "flex";
-}        
-</script>
+        function openKeluarPopup() {
+            keluar.style.display = "flex";
+        }        
+    </script>
 
 </body>
 </html>
 @endauth
 @guest
-    @php
-        header("Location: " . route('front'));
-        exit();
-    @endphp
+@php
+header("Location: " . route('front'));
+exit();
+@endphp
 @endguest
